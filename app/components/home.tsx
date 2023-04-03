4e69dae021669da8baa3d7daa76a5b1725e3f1fd
Sonic.Lee <70356411+li814222685@users.noreply.github.com>
2023-04-03 13:10:40 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -111,9 +111,9 @@ export function Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>Sonic.Lee Robot</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            体验一下聊天Gpt.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
